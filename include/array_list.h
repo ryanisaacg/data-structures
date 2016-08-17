16dd0076a580348dbf2b9311155fb457f871aeea
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-17 17:01:46 -0400

    Added an array list module

---
+++ include/array_list.h
@@ -0,0 +1,22 @@
+#ifndef ARRAY_LIST_H_
+#define ARRAY_LIST_H_
+
+#include <stdbool.h>
+
+#include "util.h"
+
+DEFSTRUCT(array_list);
+struct array_list {
+    size_t item_size;
+    int length, capacity;
+    void *buffer;
+};
+
+array_list al_new(size_t item_size);
+array_list al_new_sized(size_t item_size, int init_capacity);
+void *al_get(array_list *list, int index);
+void al_add(array_list *list, void *item);
+void al_remove(array_list *list, int index);
+void al_set(array_list *list, int index, void *data);
+int al_size(array_list *list);
+#endif
---
+++ src/array_list.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "array_list.h"
+
+array_list al_new(size_t item_size) {
+    return al_new_sized(item_size, 10);
+}
+
+array_list al_new_sized(size_t item_size, int init_capacity) {
+    array_list list = {item_size, 0, init_capacity, malloc(item_size * init_capacity)};
+    return list;
+}
+
+void *al_get(array_list *list, int index) {
+    return list->buffer + item_size * index;
+}
+
+void al_add(array_list *list, void *item) {
+    if(list->length < list->capacity) {
+        al_set(list, list->length, item);
+        list->length++;
+    } else {
+        list->capacity *= 2;
+        list->buffer = realloc(list->buffer, list->capacity * list->item_size);
+        al_add(list, item);
+    }
+}
+
+void al_remove(array_list *list, int index) {
+    memmove(list->buffer + list->item_size * index, list->buffer + list->item_size * (index + 1), list->length - index);
+    list->length -= 1;
+}
+
+void al_set(array_list *list, int index, void *data) {
+    memcpy(list->buffer + index * list->item_size, data, list->item_size);
+}
+
+int al_size(array_list *list) {
+    return list->size;
+}
