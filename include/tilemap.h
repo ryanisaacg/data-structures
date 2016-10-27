94a51af9f50eb8a534ed7a9f6a76bf0ab8a4377c
EpicPieGuy <ryanisaacg@gmail.com>
2016-10-27 06:30:00 -0400

    Added a function declaration to check if a region is empty

--- include/tilemap.h
+++ include/tilemap.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdbool.h>
 #include "array_list.h"
 #include "util.h"
 
@@ -12,3 +13,4 @@ struct tilemap {
 tilemap tl_new(size_t item_size, int width, int height, int tile_size);
 void *tl_get(int x, int y);
 void tl_set(void *source, int x, int y);
+bool tl_empty(int x, int y, int width, int height);
