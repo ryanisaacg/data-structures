61c60b75c7b4c3de88961c4bd8b14e45ad1704a5
EpicPieGuy <ryanisaacg@gmail.com>
2016-10-27 17:09:42 -0400

    Updated the tilemap so it compiles

--- include/tilemap.h
+++ include/tilemap.h
@@ -8,18 +8,20 @@ DEFSTRUCT(tilemap);
 struct tilemap {
 	const int width, height, tile;
 	const size_t item_size;
-	const char *buffer;
-	const bool *has;
+	char *buffer;
+	bool *has;
 };
 //Creates a new tilemap
 tilemap tl_new(size_t item_size, int width, int height, int tile_size);
 //Creates a new tilemap on a preallocated block of memory
 tilemap tl_prealloc(size_t item_size, int width, int height, int tile_size, char *buffer, bool *boolBuffer);
 //Gets the item from that position
-void *tl_get(int x, int y);
+void *tl_get(tilemap map, int x, int y);
 //Sets the data of an item
-void tl_set(const void *source, int x, int y);
+void tl_set(tilemap map, const void *source, int x, int y);
+//Removes whatever is in the tilemap at this point
+void tl_remove(tilemap map, int x, int y);
 //Finds if a point is free
 bool tl_free(tilemap map, int x, int y);
 //Finds if a region is empty
-bool tl_empty(int x, int y, int width, int height);
+bool tl_empty(tilemap map, int x, int y, int width, int height);
