35dd8a0dd5c010ca8cdfe49916e71ad74e999327
EpicPieGuy <ryanisaacg@gmail.com>
2016-10-27 15:17:37 -0400

    Added an item to the unordered list header file

--- include/unordered_list.h
+++ include/unordered_list.h
@@ -11,10 +11,12 @@ struct unordered_list {
 
 //Create a new unordered list
 unordered_list ul_new(size_t item_size);
+//Create a new unordered list using a pre-allocated buffer
+unordered_list ul_prealloc(size_t item_size, void *buffer, size_t buffer_size);
 //Get an item from the list (can change based on removals)
 void *ul_get(const unordered_list list, size_t index);
 //Add an item to an unordered list
-void ul_add(unordered_list *list, const void *item, size_t index);
+void ul_add(unordered_list *list, const void *item);
 //Remove an item from an unordered list
 void ul_remove(unordered_list *list, size_t index);
 //Set the value of an index in an unordered list
