c43203867950ea6dc3c31b3c1117723105d31cac
EpicPieGuy <ryanisaacg@gmail.com>
2016-10-28 06:18:55 -0400

    Renamed linked_list to llist and unoredered_list to ulist

--- include/linked_list.h
+++ include/linked_list.h
@@ -3,42 +3,42 @@
 #include "util.h"
 #include <stdbool.h>
 
-DEFSTRUCT(linked_list);
-DEFSTRUCT(linked_node);
-DEFSTRUCT(linked_iter);
+DEFSTRUCT(llist);
+DEFSTRUCT(lnode);
+DEFSTRUCT(literator);
 
-struct linked_node {
+struct lnode {
 	void *data;
-	linked_node *next, *prev;
+	lnode *next, *prev;
 };
-struct linked_list {
-	linked_node *head, *tail;
+struct llist {
+	lnode *head, *tail;
 };
-struct linked_iter {
-	linked_list *origin;
-	linked_node *current;
+struct literator {
+	llist *origin;
+	lnode *current;
 	bool goesForward;
 };
 
-linked_list *ll_new();
-linked_list *ll_duplicate(linked_list *list);
-void ll_add_first(linked_list *list, void *value);
-void ll_add_last(linked_list *list, void *value);
-void ll_concat(linked_list *mutated, linked_list *newItems);
-void *ll_get_first(linked_list *list);
-void *ll_get_last(linked_list *list);
-void *ll_get(linked_list *list, int index);
-void *ll_remove_last(linked_list *list);
-void *ll_remove_first(linked_list *list);
-bool ll_empty(linked_list *list);
-void ll_clear(linked_list *list);
-void ll_destroy(linked_list *list);
-void ll_delete_all(linked_list *list);
-int ll_size(linked_list *list);
+llist *ll_new();
+llist *ll_duplicate(llist *list);
+void ll_add_first(llist *list, void *value);
+void ll_add_last(llist *list, void *value);
+void ll_concat(llist *mutated, llist *newItems);
+void *ll_get_first(llist *list);
+void *ll_get_last(llist *list);
+void *ll_get(llist *list, int index);
+void *ll_remove_last(llist *list);
+void *ll_remove_first(llist *list);
+bool ll_empty(llist *list);
+void ll_clear(llist *list);
+void ll_destroy(llist *list);
+void ll_delete_all(llist *list);
+int ll_size(llist *list);
 
-linked_iter ll_iter_head(linked_list *list);
-linked_iter ll_iter_tail(linked_list *list);
-void *ll_iter_next(linked_iter *iter);
-bool ll_iter_has_next(linked_iter *iter);
-void ll_iter_clear_to_current(linked_iter *iter);
-void ll_iter_clear_remaining(linked_iter *iter);
+literator ll_iter_head(llist *list);
+literator ll_iter_tail(llist *list);
+void *ll_iter_next(literator *iter);
+bool ll_iter_has_next(literator *iter);
+void ll_iter_clear_to_current(literator *iter);
+void ll_iter_clear_remaining(literator *iter);
--- include/unordered_list.h
+++ include/unordered_list.h
@@ -3,21 +3,21 @@
 #include <stddef.h>
 #include "util.h"
 
-DEFSTRUCT(unordered_list);
-struct unordered_list {
+DEFSTRUCT(ulist);
+struct ulist {
 	size_t length, capacity, item;
 	char *buffer;
 };
 
 //Create a new unordered list
-unordered_list ul_new(size_t item_size);
+ulist ul_new(size_t item_size);
 //Create a new unordered list using a pre-allocated buffer
-unordered_list ul_prealloc(size_t item_size, void *buffer, size_t buffer_size);
+ulist ul_prealloc(size_t item_size, void *buffer, size_t buffer_size);
 //Get an item from the list (can change based on removals)
-void *ul_get(const unordered_list list, size_t index);
+void *ul_get(const ulist list, size_t index);
 //Add an item to an unordered list
-void ul_add(unordered_list *list, const void *item);
+void ul_add(ulist *list, const void *item);
 //Remove an item from an unordered list
-void ul_remove(unordered_list *list, size_t index);
+void ul_remove(ulist *list, size_t index);
 //Set the value of an index in an unordered list
-void ul_set(unordered_list list, const void *item, size_t index);
+void ul_set(ulist list, const void *item, size_t index);
