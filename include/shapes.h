a46d0b64c97c30230c6222215cf8fae7c9ca895c
Ryan Goldstein <ryanisaacg@gmail.com>
2016-10-28 18:41:40 -0400

    Defined functions to use with the shapes

--- include/shapes.h
+++ include/shapes.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdbool.h>
 #include "util.h"
 
 DEFSTRUCT(rect);
@@ -25,3 +26,14 @@ struct shape {
 		circle circ;
 	} data;
 };
+
+shape shape_rect(int x, int y, int width, int height);
+shape shape_circ(int x, int y, int radius);
+int shape_x(shape s);
+int shape_y(shape s);
+int shape_left(shape s);
+int shape_right(shape s);
+int shape_top(shape s);
+int shape_bottom(shape s);
+int shape_set_pos(shape *s, int x, int y);
+bool shape_overlaps(shape a, shape b); 
