34c2e72c04f7cacf7d675d74d99eae6e1a5afca5
Ryan Goldstein <ryanisaacg@gmail.com>
2016-10-28 18:37:35 -0400

    Added a circle and a generic shape container

--- include/shapes.h
+++ include/shapes.h
@@ -11,3 +11,17 @@ DEFSTRUCT(vector2);
 struct vector2 {
 	int x, y;
 };
+
+DEFSTRUCT(circle);
+struct circle {
+	int x, y, radius;
+};
+
+DEFSTRUCT(shape);
+struct shape {
+	enum { SHAPE_RECT, SHAPE_CIRC} type;
+	union {
+		rect rectangle;
+		circle circ;
+	} data;
+};
