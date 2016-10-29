7a704d88fa1865100aca31a68ec5c917f63ad1c0
EpicPieGuy <ryanisaacg@gmail.com>
2016-10-28 20:18:00 -0400

    Implemented the shape collision function

--- src/shapes.c
+++ src/shapes.c
@@ -70,7 +70,38 @@ void shape_set_pos(shape *s, int x, int y) {
 }
 
 bool shape_overlaps(shape a, shape b) {
-	return false;
+	if(a.type == SHAPE_RECT && b.type == SHAPE_RECT) { 
+		rect r1 = a.data.rectangle;
+		rect r2 = b.data.rectangle;
+		return r1.x < r2.x + r2.width && r1.x + r1.width > r2.x && r1.y < r2.y + r2.height && r1.y + r1.height > r2.y;
+	} else if(a.type == SHAPE_CIRC && b.type == SHAPE_CIRC) {
+		circle c1 = a.data.circ;
+		circle c2 = b.data.circ;
+		int x = c1.x - c2.x;
+		int y = c1.y - c2.y;
+		int radius = c1.radius + c2.radius;
+		return radius * radius >= x * x + y * y;
+	} else if(a.type == SHAPE_RECT) {
+		rect r = a.data.rectangle;
+		circle c = b.data.circ;
+		int x = c.x;
+		int y = c.y;
+		if (c.x < r.x) {
+			x = r.x;
+		} else if (c.x > r.x + r.width) {
+			x = r.x + r.width;
+		}
+		if (c.y < r.y) {
+			y = r.y;
+		} else if (c.y > r.y + r.height) {
+			y = r.y + r.height;
+		}
+		x = x - c.x;
+		y = y - c.y;
+		return x * x + y * y < c.radius * c.radius;
+	} else {
+		return shape_overlaps(b, a); //only write code to cover rect - circle
+	}
 }
 
 bool shape_contains(shape s, vector2 point) {
@@ -78,12 +109,14 @@ bool shape_contains(shape s, vector2 point) {
 	case SHAPE_RECT: {
 		rect r = s.data.rectangle;
 		return point.x >= r.x && point.y >= r.y && point.x < r.x + r.width && point.y < r.y + r.height;
-	}
+		}
 	case SHAPE_CIRC: {
-		circ c = s.data.circ;
+		circle c = s.data.circ;
 		int x = c.x - point.x;
 		int y = c.y - point.y;
-		return radius * radius >= x * x + y * y;
+		return c.radius * c.radius >= x * x + y * y;
+		}
 	}
+	return false;
 }
 
